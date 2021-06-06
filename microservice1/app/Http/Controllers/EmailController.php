e2caae9e09d2d1ada2da9ecde3aeb5c8b08dee23
Lucas Dumer <lucasdumer@gmail.com>
2021-06-06 20:30:22 -0300

    adicionado poc de publish em sns

---
+++ microservice1/app/Http/Controllers/EmailController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Aws\Sns\SnsClient;
+
+class EmailController
+{
+    public function create()
+    {
+        try {
+            $SnsClient = new SnsClient([
+                'region' => 'us-east-1',
+                'version' => '2010-03-31',
+                'endpoint' => 'http://192.168.48.1:4566/',
+                'credentials' => [
+                    'key'    => "ACCESSKEYID",
+                    'secret' => "SECRETACCESSKEY",
+                ],
+            ]);
+            $SnsClient->publish([
+                'TopicArn' => 'arn:aws:sns:us-east-1:000000000000:poc-topic-emails',
+                'Message' => 'test'
+            ]);
+        } catch(\Exception $e) {
+            dd($e->getMessage());
+        }
+    }
+}
